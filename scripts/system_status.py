f676788187945faa92df16e7de406f5eb8f53700
gjbaker <gregory.j.baker@gmail.com>
2026-02-19 17:53:22 -0700

    Simplify commands

--- scripts/system_status.py
+++ scripts/system_status.py
@@ -9,13 +9,13 @@ import os
 PS_OUT = subprocess.check_output(['ps', '-eaf']).decode('utf-8')
 
 # should already be in FreedomMailingETL directory
-UV_RUN = '/home/operations/.local/bin/uv run'
+UVR = 'uv run'
 if 'dropbox' not in PS_OUT:
     print('Starting Dropbox monitoring')
-    os.system(f'{UV_RUN} ../dropbox.py start &')
+    os.system(f'{UVR} ../dropbox.py start &')
 if 'hlp_monitor_sftp.py' not in PS_OUT:
     print('Starting Heber Light & Power SFTP monitoring')
-    os.system(f'{UV_RUN} /home/hlap/.monitor/hlp_monitor_sftp.py &')
+    os.system(f'{UVR} /home/hlap/.monitor/hlp_monitor_sftp.py &')
 if 'dispatcher.py' not in PS_OUT:
     print('Starting FreedomMailingETL monitoring of Dropbox directory.')
-    os.system(f'{UV_RUN} dispatcher.py &')
+    os.system(f'{UVR} dispatcher.py &')
