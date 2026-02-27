c1d69d1abe89eec107feae3436965e4b1e9c8634
gjbaker <gregory.j.baker@gmail.com>
2026-02-26 22:08:18 -0700

    Refactor for deprecation of os.system()

--- scripts/system_status.py
+++ scripts/system_status.py
@@ -1,21 +1,25 @@
-"""Check if required background programs for FreedomMailingETL are running."""
+"""Check if required background programs for FreedomMailingETL are running.
+
+Initiate program from the project directory (FreedomMailingETL).
+"""
 
 
 import subprocess
-import os
+import shlex
 
 
 # use subprocess to get output of program status list
 PS_OUT = subprocess.check_output(['ps', '-eaf']).decode('utf-8')
 
-# should already be in FreedomMailingETL directory
 UVR = 'uv run'
 if 'dropbox' not in PS_OUT:
     print('Starting Dropbox monitoring')
-    os.system(f'{UVR} ../dropbox.py start &')
+    subprocess.Popen(shlex.split(UVR) + ['../dropbox.py', 'start'])
+
 if 'hlp_monitor_sftp.py' not in PS_OUT:
     print('Starting Heber Light & Power SFTP monitoring')
-    os.system(f'{UVR} /home/hlap/.monitor/hlp_monitor_sftp.py &')
+    subprocess.Popen(shlex.split(UVR) + ['/home/hlap/.monitor/hlp_monitor_sftp.py'])
+
 if 'dispatcher.py' not in PS_OUT:
     print('Starting FreedomMailingETL monitoring of Dropbox directory.')
-    os.system(f'{UVR} dispatcher.py &')
+    subprocess.Popen(shlex.split(UVR) + ['dispatcher.py'])
